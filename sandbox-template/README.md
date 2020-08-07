0cfa41dd6e20fe912c96c1eb8fc95c38d205a0fb
tastethedream <tastethedream@gmail.com>
2020-08-07 18:54:56 +0100

    sandbox envirmonment created for testing

---
+++ sandbox-template/README.md
@@ -0,0 +1,56 @@
+# cloud-hero-landing-page
+
+## Mission
+
+Update the example landing page to accept custom fields:
+
+* Title
+* Subtitle
+* URL
+* Access Code
+
+## Requirements:
+
+* Code be based on the existing template in the repository.
+* Runtime Language(s) - HTML, CSS and Javascript (Node/Express)
+* Changes to the repo should not be made to the mainline branch
+* Raise a PR for code approval to instigate merge to the mainline
+
+
+## Making a change:
+
+* Pull the latest code
+```
+git pull
+```
+
+* Checkout a new branch
+```
+git checkout -b myNewBranch
+```
+
+* Status of the staging area
+``` 
+git status
+```
+
+* Add changed code 
+```
+git add [FILE]
+```
+
+* Commit a message 
+
+Be a good citizen and use nice commit messages
+```
+git commit -m [What + Why change needed]
+```
+
+* Push code to the remote repository
+```
+git push origin [BRANCH]
+```
+
+
+## Example landing page
+https://go.qwiklabs.com/cloud-hero-classic-challenge-paypal-singapore
---
+++ sandbox-template/public/scripts/app.js
@@ -0,0 +1,211 @@
+
+    // Create Dino Constructor
+    function Dinosaur(species, weight, height, diet, where, when, fact, image ){
+      this.species = species;
+      this.weight = weight;
+      this.height = height;
+      this.diet = diet;
+      this.where = where;
+      this.when = when;
+      this.fact = fact;
+      this.image = image;
+  
+  };
+  
+      // Create Dino Objects
+      // should the facts be in an array with some empty ones ??//
+     
+  const pigeon = new Dinosaur(
+      'Pigeon',
+       0.5,
+        9,
+      'herbevor',
+      'World Wide', 
+      'Holocene',
+      'all birds are considered dinosaurs',
+      'images/pigeon.png'
+  );
+  
+  const pteranadon = new Dinosaur(
+      'Pteranodon',
+       44,
+       20, 
+       'carnivor',
+       'North America',
+       'Late Cretaceous',
+       'images/pteranodon.png'
+  );
+
+  
+  const elasmosaurus = new Dinosaur(
+      'Elasmosaurus',
+       16000,
+        59, 
+      'carnivor',
+      'North America',
+      'Late Cretaceous',
+      'A marine repltile, first discovered in kansas',
+     'images/elasmosaurus.png'
+  );
+  
+  const stegosaurus = new Dinosaur(
+      'Stegosaurus',
+       11600,
+       79, 
+       'herbevor',
+      'North America, Europe, Asia',
+      'Late Jurasic to Early Cretaceous',
+      'The Stegosaurus had between 17 and 22 seperate places and flat spines.', 
+      'images/stegosaurus.png'
+  );
+  
+  const brachiosaurus = new Dinosaur(
+      'Brachiosaurus',
+       70000,
+       372, 
+       'herbevor', 
+       'North America',
+       'Late Jurasic',
+       'An asteroid was named 9954 Brachiosaurus in 1991',
+       'images/brachiosaurus.png'
+  );
+  
+  const tyrannosaurus = new Dinosaur(
+      'Tyrannosaurus Rex',
+       11905,
+       144, 
+       'carnivor',
+       'North America',
+       'Late Cretaceous',
+       'It has a skull measuring 5 feet long',
+       'images/tyrannosaurus rex.png'
+  );
+  
+  const  triceratops = new Dinosaur(
+      'Triceratops',
+       13000, 
+       114, 
+       'herbevor', 
+       'North America',
+       'Late Cretaceous',
+       'First discovered in 1889 by Othanial Charles Marsh',
+       'images/triceratops.png'
+  );
+  
+  const anklyosaurus = new Dinosaur(
+      'Anklyosaurus',
+       10500,
+       55, 
+       'herbevor',
+       'North America',
+       'Late Cretaceous',
+       'Survived for approximately 135 million years',
+       'images/anklyosaurus.png'
+  );
+  
+  console.log (pigeon);
+
+
+  // Create Human Object
+  // not sure if i need this????
+
+  
+  function Human(name, weight, feet, inches, diet){
+      this.name = name;
+      this.weight = weight;
+      this.feet = feet;
+      this.inches = inches;
+      this.diet = diet;
+  }
+
+  
+  // Use IIFE to get human data from form
+
+  function humanData(){
+      const human =(function(){
+          let name = document.getElementById('name').value;
+          let weight = document.getElementById('weight').value;
+          let feet = document.getElementById('feet').value;
+          let inches = document.getElementById('inches').value;
+          let diet = document.getElementById('diet').value;
+          let image = 'images/human.png';
+
+          function getName(){
+           return name;
+          }
+
+          function getWeight(){
+              return weight;
+             }
+
+          function getHeight(){
+              return parseFloat((feet) * 12) + parseFloat(inches);;
+             }   
+
+          function getDiet(){
+              return diet;
+             }
+
+             function getImage(){
+              return image;
+             }
+
+             return {
+                 name: getName(),
+                 weight: getWeight(),
+                 height: getHeight(),
+                 diet: getDiet(),
+                 image: getImage()
+             };
+      })();
+
+      return human;
+      console.log(human);
+  }
+
+  
+
+  // Create Dino Compare Method 1
+  // NOTE: Weight in JSON file is in lbs, height in inches. 
+  // Compare diet
+  // will this.name show human name or dino name???
+  // not sure about humanDiet how to point to that????
+  Dinosaur.prototype.compareDiet = function(humanDiet){
+      if (this.diet === humanDiet){
+          this.dietFact = 'This dinosaur has the same diet as ${this.name}';
+      } else {
+          this.dietFact = 'This dinosaur was a ${this.diet}';
+      }
+   };
+  
+
+
+  
+  // Create Dino Compare Method 2
+  // NOTE: Weight in JSON file is in lbs, height in inches.
+  // compare weight
+  Dinosaur.prototype.compareWeight = function(humanWeight){
+      this.weightFact = 'This dinosaur was ${math.floor(this.weight/humanWeight)} times heavier than you';      
+   };
+  
+
+
+  
+  // Create Dino Compare Method 3
+  // NOTE: Weight in JSON file is in lbs, height in inches.
+  //compare height
+  Dinosaur.prototype.compareHeight = function(humanHeight){
+      this.heightFact = 'This dinosaur was ${math.floor(this.height/humanheight)} times taller than you';      
+   };
+  
+
+
+  // Generate Tiles for each Dino in Array
+  // map()????
+
+      // Add tiles to DOM
+
+  // Remove form from screen
+
+
+// On button click, prepare and display infographic
